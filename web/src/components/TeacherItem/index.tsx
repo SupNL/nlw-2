3f76ef8810898ab1a86ab851d49c710decb95b56
Gustavo Patara <gustavoonishinl@gmail.com>
2020-08-06 20:27:10 -0300

    frontend: api connection & finishing

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,35 +2,53 @@ import React from "react";
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from "../../services/api";
 
-const TeacherItem = () => {
+export interface Teacher {
+  id : number;
+  name : string;
+  avatar : string;
+  whatsapp : string;
+  bio : string;
+  subject : string;
+  cost : number;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem : React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection(){
+    api.post('/connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img
-          src="https://avatars3.githubusercontent.com/u/44099109?s=460&u=0666901ac0fd47bd408d0d32b1d636c353946e78&v=4"
-          alt="Gustavo Patara"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Gustavo Patara</strong>
-          <span>Tecnologia da Informação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Wooow dou aula de ti quem diria
-        <br />
-        <br />
-        nl o cara brabo tentando dar aula eaeaeaeae
+        {teacher.bio}
       </p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
